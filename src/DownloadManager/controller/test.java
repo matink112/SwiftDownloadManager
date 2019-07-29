06e2ed20f7061be8a5a394bc8c6665e54670d30f
matink112 <matink112@gmail.com>
2019-07-29 13:11:14 +0430

    update downloading page ui

---
+++ src/DownloadManager/controller/test.java
@@ -0,0 +1,23 @@
+package DownloadManager.controller;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.OpenOption;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.FileAttribute;
+
+public class test {
+
+
+
+    public static void main(String[] args) throws IOException {
+
+        System.out.println(System.getProperty("user.dir"));
+
+        //Path p = Paths.get();
+//        Files.exists(p);
+//        Files.createFile(p );
+    }
+
+}
